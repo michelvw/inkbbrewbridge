11bbbef57b89616c01d55d190b31149f190a7d63
michelvw <michel@vanwesten.org>
2021-01-06 09:31:08 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+db_file             = r"temperatures.db" #Name and location of .db file, default temperatures.db
+get_url             = "" #URL where current temperature reading from Inkbird can be retreived
+post_url            = "http://log.brewfather.net/stream?id=" #Your Brewfather Post URL, normally starting with http://log.brewfather.net/stream?id=
+upload_frequency    = 60*15 #in seconds (brewfather minumum is 15 minutes, 60*15)
